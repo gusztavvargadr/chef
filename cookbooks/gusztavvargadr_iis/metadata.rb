f766646649d0f21d9817c27cd220dc4420efa17a
Gusztáv Varga <me@gusztavvargadr.me>
2017-03-05 14:42:27 +0100

    #18 Use user and password instead of userinfo

--- cookbooks/gusztavvargadr_iis/metadata.rb
+++ cookbooks/gusztavvargadr_iis/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'me@gusztavvargadr.me'
 license 'MIT'
 description 'Installs/Configures IIS'
 long_description 'Installs/Configures IIS'
-version '0.2.0'
+version '0.2.2'
 
 depends 'gusztavvargadr_windows'
