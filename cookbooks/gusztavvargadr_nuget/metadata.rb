ae3e8da4cb071f33b50f872a0906d6230400bf6a
Gusztáv Varga <me@gusztavvargadr.me>
2017-04-09 11:14:41 +0200

    #24 Remove Git, SVN and NuGet cookbooks

--- cookbooks/gusztavvargadr_nuget/metadata.rb
+++
@@ -1,7 +0,0 @@
-name 'gusztavvargadr_nuget'
-maintainer 'Gusztav Varga'
-maintainer_email 'me@gusztavvargadr.me'
-license 'MIT'
-description 'Installs/Configures NuGet'
-long_description 'Installs/Configures NuGet'
-version '0.2.2'
