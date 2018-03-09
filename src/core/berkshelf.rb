736f7bd58e880af82cbdd4f8160e929b4a9087b9
Gusztáv Varga <me@gusztavvargadr.me>
2018-03-09 12:22:12 +0100

    Refactor cookbook folders #32

--- src/core/berkshelf.rb
+++ src/core/berkshelf.rb
@@ -2,14 +2,14 @@ def gusztavvargadr_sources
   source 'https://supermarket.chef.io'
 end
 
-def gusztavvargadr_cookbook(component, namespace = '', path = '')
-  cookbook_name = "gusztavvargadr_#{component}"
+def gusztavvargadr_chef_cookbook(component = '', namespace = '')
+  cookbook_name = 'gusztavvargadr'
+  cookbook_name = "#{cookbook_name}_#{component}" unless component.to_s.empty?
   cookbook_name = "#{cookbook_name}_#{namespace}" unless namespace.to_s.empty?
 
-  if path.to_s.empty?
-    path = namespace.to_s.empty? ? 'core' : namespace
-  end
-  cookbook_path = "#{File.dirname(__FILE__)}/../#{component}/#{path}"
+  path = component.to_s.empty? ? 'core' : component
+  path = "#{path}/cookbooks/#{namespace.to_s.empty? ? 'core' : namespace}"
+  cookbook_path = "#{File.dirname(__FILE__)}/../#{path}"
 
   cookbook cookbook_name, path: cookbook_path
 end
