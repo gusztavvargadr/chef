5a67a1cbe4c28b678735ad3fd860dc9d09bd29f2
Gusztáv Varga <me@gusztavvargadr.me>
2018-06-23 10:38:38 +0200

    Add initial Policiyfile samples #32

---
+++ samples/policies/Policyfile.windows_test_chocolatey_packages.rb
@@ -0,0 +1,9 @@
+name 'windows_test_chocolatey_packages'
+run_list 'recipe[gusztavvargadr_windows_test::chocolatey_packages]'
+
+default_source :supermarket
+default_source :chef_repo, '/vagrant-chef-cookbooks'
+
+cookbook 'gusztavvargadr_windows_test'
+cookbook 'gusztavvargadr_windows'
+cookbook 'windows'
--- src/kitchen.vagrant.rb
+++ src/kitchen.vagrant.rb
@@ -1,4 +1,4 @@
-require "#{File.dirname(__FILE__)}/../lib/gusztavvargadr/vagrant/src/vagrant"
+require "#{File.dirname(__FILE__)}/vagrant"
 
 VagrantMachine.defaults(
   'providers' => {
