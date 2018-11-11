3bbcd90fba7c1a717e1176fab5c53ab0e5dc9e2c
Gusztáv Varga <human@gusztavvargadr.me>
2018-11-11 19:12:37 +0100

    Refactor Docker installation #42

---
+++ samples/cookbooks/docker/attributes/engine_community.rb
@@ -0,0 +1,12 @@
+default['gusztavvargadr_docker']['engine_community'] = {
+  'features' => {
+    'Containers' => {},
+    'Hyper-V' => {},
+  },
+  'powershell_modules' => {},
+  'chocolatey_packages' => {
+    'docker-for-windows' => {
+      'elevated' => true,
+  },
+  'powershell_packages' => {},
+}
