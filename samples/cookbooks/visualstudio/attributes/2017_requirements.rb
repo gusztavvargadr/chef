56f6397d08de4366cf5ddacf77def1208b1de82d
Gusztáv Varga <mail@gusztavvargadr.me>
2019-10-01 20:46:09 +0200

    Fix Visual Studio requirements #38

--- samples/cookbooks/visualstudio/attributes/2017_requirements.rb
+++ samples/cookbooks/visualstudio/attributes/2017_requirements.rb
@@ -2,4 +2,14 @@ default['gusztavvargadr_visualstudio']['2017_requirements'] = {
   'features' => {
     'NetFx3$' => {},
   },
+  'native_packages' => {
+    '.NET Core 2.2.7 SDK' => {
+      'source' => 'https://download.visualstudio.microsoft.com/download/pr/f4fc2718-bc21-4a97-ac01-426aed568946/a5f6b770c7396ad1eb71355e4dc234f6/dotnet-sdk-2.2.109-win-x64.exe',
+      'install' => [
+        '/install',
+        '/quiet',
+        '/norestart',
+      ],
+    },
+  },
 }
--- samples/cookbooks/visualstudio/attributes/2019_requirements.rb
+++ samples/cookbooks/visualstudio/attributes/2019_requirements.rb
@@ -2,4 +2,14 @@ default['gusztavvargadr_visualstudio']['2019_requirements'] = {
   'features' => {
     'NetFx3$' => {},
   },
+  'native_packages' => {
+    '.NET Core 2.2.7 SDK' => {
+      'source' => 'https://download.visualstudio.microsoft.com/download/pr/40c1dd82-671c-4974-919d-ac8a61ef5a91/49ab67c335878f4a5bdd84e14c76708f/dotnet-sdk-2.2.402-win-x64.exe',
+      'install' => [
+        '/install',
+        '/quiet',
+        '/norestart',
+      ],
+    },
+  },
 }
