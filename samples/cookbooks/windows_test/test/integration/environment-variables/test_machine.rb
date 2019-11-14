4cfd0cc8117c7680e2c8bbba145a6c613b018f32
Gusztáv Varga <mail@gusztavvargadr.me>
2019-11-14 06:53:08 +0100

    Fix Windows cookbook test cases #38

--- samples/cookbooks/windows_test/test/integration/environment-variables/test_machine.rb
+++ samples/cookbooks/windows_test/test/integration/environment-variables/test_machine.rb
@@ -1,5 +1,5 @@
-value_script_code = '$env:VARIABLE_TEST'
-value_script_result = 'test'
+value_script_code = '$env:TEST_MACHINE'
+value_script_result = 'Test Machine'
 
 describe powershell(value_script_code) do
   its('stdout') { should include value_script_result }
