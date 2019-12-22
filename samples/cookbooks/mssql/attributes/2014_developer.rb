a3048d4c8ff34abede15d748ef0f4c4706a0bdbc
Gusztáv Varga <mail@gusztavvargadr.me>
2019-12-22 12:29:08 +0100

    Remove obsolete 2014 version and add AZP pipeline #48

--- samples/cookbooks/mssql/attributes/2014_developer.rb
+++
@@ -1,5 +0,0 @@
-default['gusztavvargadr_mssql']['2014_developer'] = {
-  'installer_iso_url' => "#{ENV['USERPROFILE']}/Downloads/en_sql_server_2014_developer_edition_with_service_pack_2_x64_dvd_8967821.iso",
-  'patch_file_url' => 'https://download.microsoft.com/download/7/9/F/79F4584A-A957-436B-8534-3397F33790A6/SQLServer2014SP3-KB4022619-x64-ENU.exe',
-  # 'patch_file_url' => 'https://download.microsoft.com/download/A/5/A/A5AACC94-29A5-4890-90BD-847320EE0E93/SQLServer2014-KB4491539-x64.exe',
-}
--- samples/cookbooks/mssql/recipes/2014_requirements.rb
+++
@@ -1,3 +0,0 @@
-gusztavvargadr_windows_features '' do
-  options node['gusztavvargadr_mssql']['2014_requirements']['features']
-end
