14e41a6b31c3e59b805a5a4c87fad01d89f626d0
Gusztáv Varga <mail@gusztavvargadr.me>
2019-12-22 01:47:08 +0100

    Update .NET Core SDKs and add AZP pipeline #49

--- samples/cookbooks/visualstudio/attributes/2019_requirements.rb
+++ samples/cookbooks/visualstudio/attributes/2019_requirements.rb
@@ -3,8 +3,16 @@ default['gusztavvargadr_visualstudio']['2019_requirements'] = {
     'NetFx3$' => {},
   },
   'native_packages' => {
-    '.NET Core 2.2.7 SDK' => {
-      'source' => 'https://download.visualstudio.microsoft.com/download/pr/40c1dd82-671c-4974-919d-ac8a61ef5a91/49ab67c335878f4a5bdd84e14c76708f/dotnet-sdk-2.2.402-win-x64.exe',
+    '.NET Core 2.2.8 SDK' => {
+      'source' => 'https://download.visualstudio.microsoft.com/download/pr/279de74e-f7e3-426b-94d8-7f31d32a129c/e83e8c4c49bcb720def67a5c8fe0d8df/dotnet-sdk-2.2.207-win-x64.exe',
+      'install' => [
+        '/install',
+        '/quiet',
+        '/norestart',
+      ],
+    },
+    '.NET Core 3.0.1 SDK' => {
+      'source' => 'https://download.visualstudio.microsoft.com/download/pr/66adfd75-9c1d-4e44-8d9c-cdc0cbc41104/5288b628601e30b0fa10d64fdaf64287/dotnet-sdk-3.0.101-win-x64.exe',
       'install' => [
         '/install',
         '/quiet',
