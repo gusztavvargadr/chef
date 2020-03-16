042d97204c28a887473f0142386e427e7c3af0f1
Gusztáv Varga <mail@gusztavvargadr.me>
2020-03-16 21:31:17 +0100

    Update SQL Server patches #56

--- samples/cookbooks/mssql/attributes/2017_developer.rb
+++ samples/cookbooks/mssql/attributes/2017_developer.rb
@@ -1,4 +1,4 @@
 default['gusztavvargadr_mssql']['2017_developer'] = {
   'installer_iso_url' => 'https://download.microsoft.com/download/E/F/2/EF23C21D-7860-4F05-88CE-39AA114B014B/SQLServer2017-x64-ENU-Dev.iso',
-  'patch_file_url' => 'https://download.microsoft.com/download/C/4/F/C4F908C9-98ED-4E5F-88D5-7D6A5004AEBD/SQLServer2017-KB4535007-x64.exe',
+  'patch_file_url' => 'http://download.windowsupdate.com/c/msdownload/update/software/updt/2020/02/sqlserver2017-kb4535007-x64_6c883a7a36a1029066e2be6ab9eeab0967804580.exe',
 }
