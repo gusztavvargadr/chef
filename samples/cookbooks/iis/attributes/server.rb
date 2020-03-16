2ebec7710d1ce3cd1cb1523df582be8c23fd5438
Gusztáv Varga <mail@gusztavvargadr.me>
2020-03-16 21:23:18 +0100

    Update .NET Core components for IIS and Visual Studio #56

--- samples/cookbooks/iis/attributes/server.rb
+++ samples/cookbooks/iis/attributes/server.rb
@@ -11,24 +11,16 @@ default['gusztavvargadr_iis']['server'] = {
         '/norestart',
       ],
     },
-    '.NET Core 2.1.15 Hosting Bundle' => {
-      'source' => 'https://download.visualstudio.microsoft.com/download/pr/633b17e5-a489-4da4-9713-5ddedf17a5f0/5c18f4203e837dd90ba3da59eee92b01/dotnet-hosting-2.1.15-win.exe',
+    '.NET Core 2.1.16 Hosting Bundle' => {
+      'source' => 'https://download.visualstudio.microsoft.com/download/pr/5a059308-c27a-4223-b04d-0e815dce2cd0/10f528c237fed56192ea22283d81c409/dotnet-hosting-2.1.16-win.exe',
       'install' => [
         '/install',
         '/quiet',
         '/norestart',
       ],
     },
-    '.NET Core 3.0.2 Hosting Bundle' => {
-      'source' => 'https://download.visualstudio.microsoft.com/download/pr/7333c58c-6aa8-4dc2-9c1c-8116f18298ee/4e987f142794d8949e79344f42e253e6/dotnet-hosting-3.0.2-win.exe',
-      'install' => [
-        '/install',
-        '/quiet',
-        '/norestart',
-      ],
-    },
-    '.NET Core 3.1.1 Hosting Bundle' => {
-      'source' => 'https://download.visualstudio.microsoft.com/download/pr/c9206d6d-a11a-4b0b-834b-6320c44d0a2d/993571f75a96b6a64f8bca001797c4f0/dotnet-hosting-3.1.1-win.exe',
+    '.NET Core 3.1.2 Hosting Bundle' => {
+      'source' => 'https://download.visualstudio.microsoft.com/download/pr/dd119832-dc46-4ccf-bc12-69e7bfa61b18/990843c6e0cbd97f9df68c94f6de6bb6/dotnet-hosting-3.1.2-win.exe',
       'install' => [
         '/install',
         '/quiet',
