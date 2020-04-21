59f7251b013d63be9306bc1b2d8ce815f8c7e0be
Gusztáv Varga <mail@gusztavvargadr.me>
2020-04-21 18:54:17 +0200

    Convert all Windows cookbook tests #54

--- samples/cookbooks/windows_test/test/integration/chocolatey-packages/far.rb
+++ samples/cookbooks/windows_test/test/integration/chocolatey-packages/far.rb
@@ -1,5 +1,5 @@
-version_script_code = 'git'
-version_script_result = 'usage: git'
+version_script_code = 'far -h'
+version_script_result = 'Far Manager, version'
 
 describe powershell(version_script_code) do
   its('stdout') { should include version_script_result }
