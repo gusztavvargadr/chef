bb525e6f59c8813028e0491abd065ca57b529fd1
Gusztáv Varga <mail@gusztavvargadr.me>
2020-06-05 20:45:21 +0200

    Add support for WSL2 for Docker #59

--- samples/cookbooks/docker/attributes/engine_community.rb
+++ samples/cookbooks/docker/attributes/engine_community.rb
@@ -6,10 +6,25 @@ default['gusztavvargadr_docker']['engine_community'] = {
     'Hyper-V' => {
       'reboot' => true,
     },
+    'Microsoft-Windows-Subsystem-Linux' => {
+      'reboot' => true,
+    },
+    'VirtualMachinePlatform' => {
+      'reboot' => true,
+    },
   },
   'powershell_modules' => {},
   'powershell_packages' => {},
-  'native_packages' => {},
+  'native_packages' => {
+    'WSL Linux kernel' => {
+      'source' => 'https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi',
+      'install' => [
+        '/quiet',
+        '/norestart',
+      ],
+      'elevated' => true,
+    },
+  },
   'chocolatey_packages' => {
     'docker-desktop' => {
       'version' => '2.3.0.3',
