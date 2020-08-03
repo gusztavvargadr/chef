9744c592fbbe812cb64488811f27f5bb4639b702
Gusztáv Varga <mail@gusztavvargadr.me>
2020-08-03 13:00:06 +0200

    Update Docker references #65

--- samples/cookbooks/docker/attributes/engine_enterprise.rb
+++ samples/cookbooks/docker/attributes/engine_enterprise.rb
@@ -18,7 +18,7 @@ default['gusztavvargadr_docker']['engine_enterprise'] = {
   'native_packages' => {},
   'chocolatey_packages' => {
     'docker-compose' => {
-      'version' => '1.26.0',
+      'version' => '1.26.2',
     },
     'docker-machine' => {
       'version' => '0.16.2',
