cb0c9cf71e6ca6b6cfa5b9525744cb92d5d09af1
Gusztáv Varga <mail@gusztavvargadr.me>
2020-08-05 01:03:37 +0200

    Update MSSQL versions #65

--- samples/cookbooks/mssql/attributes/2019_developer.rb
+++ samples/cookbooks/mssql/attributes/2019_developer.rb
@@ -1,4 +1,4 @@
 default['gusztavvargadr_mssql']['2019_developer'] = {
   'install_file_url' => 'https://download.microsoft.com/download/7/c/1/7c14e92e-bdcb-4f89-b7cf-93543e7112d1/SQLServer2019-x64-ENU-Dev.iso',
-  'patch_file_url' => 'http://download.windowsupdate.com/c/msdownload/update/software/updt/2020/06/sqlserver2019-kb4552255-x64_c6a0778132b00ced30f06ee61875d58d7a7a70b2.exe',
+  'patch_file_url' => 'http://download.windowsupdate.com/d/msdownload/update/software/updt/2020/08/sqlserver2019-kb4563110-x64_505c6d0a8773909e87a0456978ffb43449a92309.exe',
 }
