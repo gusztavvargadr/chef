f0197d6de518126d84a7a4e3bc3d86842e1cafff
Gusztáv Varga <mail@gusztavvargadr.me>
2020-10-15 00:14:42 +0200

    Update Chef Client and Workstation vesions #72

--- build/docker/chef-client.windows.ps1
+++ build/docker/chef-client.windows.ps1
@@ -1,5 +1,5 @@
 Write-Host "Install Chef Client"
-. { iwr -useb https://omnitruck.chef.io/install.ps1 } | iex; install -project chef -version 16.4.41
+. { iwr -useb https://omnitruck.chef.io/install.ps1 } | iex; install -project chef -version 16.5.77
 
 Write-Host "Clean up"
 rm -Recurse -Force $env:TEMP/*
