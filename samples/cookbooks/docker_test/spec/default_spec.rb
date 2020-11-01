00a365c4bc60dea4a5649cb56e3963ba05531641
Gusztáv Varga <mail@gusztavvargadr.me>
2020-11-01 23:21:13 +0100

    Fix Docker tests #74

--- samples/cookbooks/docker_test/spec/default_spec.rb
+++ samples/cookbooks/docker_test/spec/default_spec.rb
@@ -3,5 +3,9 @@ require 'chefspec'
 describe 'gusztavvargadr_docker_test::default' do
   platform 'windows'
 
-  it { is_expected.to include_recipe('gusztavvargadr_docker::default') }
+  it {
+    allow_any_instance_of(Chef::Resource).to receive(:reboot_pending?).and_return(true)
+
+    is_expected.to include_recipe('gusztavvargadr_docker::default')
+  }
 end
--- samples/cookbooks/docker_test/spec/images_spec.rb
+++ samples/cookbooks/docker_test/spec/images_spec.rb
@@ -3,6 +3,10 @@ require 'chefspec'
 describe 'gusztavvargadr_docker_test::images' do
   platform 'windows'
 
-  it { is_expected.to include_recipe('gusztavvargadr_docker::engine_enterprise') }
-  it { is_expected.to include_recipe('gusztavvargadr_docker::images') }
+  it {
+    allow_any_instance_of(Chef::Resource).to receive(:reboot_pending?).and_return(true)
+
+    is_expected.to include_recipe('gusztavvargadr_docker::engine_enterprise')
+    is_expected.to include_recipe('gusztavvargadr_docker::images')
+  }
 end
