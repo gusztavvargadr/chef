c5f5618c9ece1d8ebd051862cd014325d817fe67
Gusztáv Varga <mail@gusztavvargadr.me>
2020-11-21 10:52:28 +0100

    Add missing dependency for Docker Desktop #76

--- samples/cookbooks/docker/attributes/engine_community.rb
+++ samples/cookbooks/docker/attributes/engine_community.rb
@@ -16,6 +16,13 @@ default['gusztavvargadr_docker']['engine_community'] = {
   'powershell_modules' => {},
   'powershell_packages' => {},
   'native_packages' => {
+    '.NET Framework 4.8 Runtime' => {
+      'source' => 'https://download.visualstudio.microsoft.com/download/pr/7afca223-55d2-470a-8edc-6a1739ae3252/c9b8749dd99fc0d4453b2a3e4c37ba16/ndp48-web.exe',
+      'install' => [
+        '/q',
+        '/norestart',
+      ],
+    },
     'WSL Linux kernel' => {
       'source' => 'https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi',
       'install' => [
