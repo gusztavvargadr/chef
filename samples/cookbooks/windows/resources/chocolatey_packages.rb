9839c8b466d0add49d8630e051381088cd2a7656
Gusztáv Varga <mail@gusztavvargadr.me>
2021-02-19 15:07:41 +0100

    Consolidate cookbook tests #74

--- samples/cookbooks/windows/resources/chocolatey_packages.rb
+++ samples/cookbooks/windows/resources/chocolatey_packages.rb
@@ -3,8 +3,6 @@ property :options, Hash, required: true
 default_action :install
 
 action :install do
-  return if new_resource.options.nil?
-
   new_resource.options.each do |name, options|
     gusztavvargadr_windows_chocolatey_package name do
       options options
