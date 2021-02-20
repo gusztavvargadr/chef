229ab00d0bd451c1e18c170c4513952a7a1232fc
Gusztáv Varga <mail@gusztavvargadr.me>
2021-02-20 16:09:23 +0100

    Fix Windows linting issues #74

--- samples/cookbooks/windows/resources/powershell_script_elevated.rb
+++ samples/cookbooks/windows/resources/powershell_script_elevated.rb
@@ -30,7 +30,7 @@ action :run do
   end
 
   windows_task_name = script_name
-  windows_task_command = "%SystemRoot%\\System32\\WindowsPowerShell\\v1.0\\powershell.exe -NoProfile -ExecutionPolicy Bypass -File ""#{script_file_path}"""
+  windows_task_command = "%SystemRoot%\\System32\\WindowsPowerShell\\v1.0\\powershell.exe -NoProfile -ExecutionPolicy Bypass -File \"#{script_file_path}\""
 
   if new_resource.user.to_s.empty?
     windows_task windows_task_name do
