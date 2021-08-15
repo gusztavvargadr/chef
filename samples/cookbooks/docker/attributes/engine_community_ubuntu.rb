a467285d18d90a79a5ce80c8c66fb35416be3df1
Gusztáv Varga <mail@gusztavvargadr.me>
2021-08-15 20:31:39 +0000

    Consolidate Docker cookbook resources #85

---
+++ samples/cookbooks/docker/attributes/engine_community_ubuntu.rb
@@ -0,0 +1,8 @@
+default['gusztavvargadr_docker']['engine']['community']['ubuntu'] = {
+  'docker-ce' => {
+    'version' => '20.10.7',
+  },
+  'docker-compose' => {
+    'version' => '1.29.2',
+  }
+}
--- samples/cookbooks/docker/resources/engine_ubuntu.rb
+++ samples/cookbooks/docker/resources/engine_ubuntu.rb
@@ -8,7 +8,6 @@ property :edition, String, default: ''
 default_action :install
 
 action :prepare do
-  return if new_resource.version.to_s.empty?
   return unless new_resource.edition.to_s == 'community'
 
   %w(apt-transport-https ca-certificates curl gnupg lsb-release).each do |package_name|
@@ -19,8 +18,9 @@ action :prepare do
 end
 
 action :install do
-  return if new_resource.version.to_s.empty?
   return unless new_resource.edition.to_s == 'community'
+
+  options = node['gusztavvargadr_docker']['engine']["#{new_resource.edition}"]['ubuntu']
 
   apt_repository 'docker' do
     arch 'amd64'
@@ -30,15 +30,29 @@ action :install do
     action :add
   end
 
-  docker_version = "5:#{new_resource.version}~3-0~ubuntu-#{shell_out('lsb_release -cs').stdout.strip}"
+  docker_ce_version = "5:#{options['docker-ce']['version']}~3-0~ubuntu-#{shell_out('lsb_release -cs').stdout.strip}"
   apt_package 'docker-ce' do
     action :install
-    version docker_version
+    version docker_ce_version
   end
 
   group 'docker' do
     append true
     members shell_out('echo ${SUDO_USER:-${USER}}').stdout.strip
+    action :create
+  end
+
+  docker_compose_version = options['docker-compose']['version']
+  docker_compose_source = "https://github.com/docker/compose/releases/download/#{docker_compose_version}/docker-compose-#{shell_out('uname -s').stdout.strip}-#{shell_out('uname -m').stdout.strip}"
+  remote_file '/usr/local/bin/docker-compose' do
+    source docker_compose_source
+    mode '0755'
+    action :create
+  end
+
+  docker_compose_completion_source = "https://raw.githubusercontent.com/docker/compose/#{docker_compose_version}/contrib/completion/bash/docker-compose"
+  remote_file '/etc/bash_completion.d/docker-compose' do
+    source docker_compose_completion_source
     action :create
   end
 end
