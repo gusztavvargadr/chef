d826a1109e6efb79c67d2fd0a3b40b28780a7f0d
Gusztáv Varga <mail@gusztavvargadr.me>
2021-12-15 13:28:21 +0100

    Update Docker versions #114

--- samples/cookbooks/docker/attributes/engine_community_ubuntu.rb
+++ samples/cookbooks/docker/attributes/engine_community_ubuntu.rb
@@ -1,6 +1,6 @@
 default['gusztavvargadr_docker']['engine']['community']['ubuntu'] = {
   'docker-ce' => {
-    'version' => '20.10.11',
+    'version' => '20.10.12',
   },
   'docker-compose' => {
     'version' => '1.29.2',
