8814a868ec987702b0796a5f13f6592080572c8a
Gusztáv Varga <mail@gusztavvargadr.me>
2021-12-15 13:49:31 +0100

    Update MSSQL versions #114

--- samples/cookbooks/mssql/attributes/server_2019_developer.rb
+++ samples/cookbooks/mssql/attributes/server_2019_developer.rb
@@ -1,5 +1,5 @@
 default['gusztavvargadr_mssql']['server']['2019_developer'] = {
   'chocolatey_packages' => {},
   'install_file_url' => 'https://download.microsoft.com/download/7/c/1/7c14e92e-bdcb-4f89-b7cf-93543e7112d1/SQLServer2019-x64-ENU-Dev.iso',
-  'patch_file_url' => 'http://download.windowsupdate.com/d/msdownload/update/software/updt/2021/10/sqlserver2019-kb5005679-x64_a6e4ec3e93be461565550973f8b8cdfdd5580967.exe',
+  'patch_file_url' => 'http://download.windowsupdate.com/d/msdownload/update/software/updt/2021/11/sqlserver2019-kb5007182-x64_f8b5c3398e9f347701066deb388c2fcd2d94eb31.exe',
 }
--- samples/cookbooks/mssql/test/integration/server-2019-developer/patch_version.rb
+++ samples/cookbooks/mssql/test/integration/server-2019-developer/patch_version.rb
@@ -1,4 +1,4 @@
 describe command('sqlcmd -Q "SELECT @@VERSION"') do
-  its('stdout') { should include 'CU13' }
-  its('stdout') { should include '15.0.4178.1' }
+  its('stdout') { should include 'CU14' }
+  its('stdout') { should include '15.0.4188.2' }
 end
