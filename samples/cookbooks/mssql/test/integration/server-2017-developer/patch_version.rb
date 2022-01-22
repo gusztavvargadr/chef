9ea5fd972cf53a163d971df35886c0bff68bf361
Gusztáv Varga <mail@gusztavvargadr.me>
2022-01-23 00:02:03 +0100

    Update pending cookbooks #126

--- samples/cookbooks/mssql/test/integration/server-2017-developer/patch_version.rb
+++ samples/cookbooks/mssql/test/integration/server-2017-developer/patch_version.rb
@@ -1,4 +1,4 @@
 describe command('sqlcmd -Q "SELECT @@VERSION"') do
-  its('stdout') { should include 'CU27' }
-  its('stdout') { should include '14.0.3421.10' }
+  its('stdout') { should include 'CU28' }
+  its('stdout') { should include '14.0.3430.2' }
 end
