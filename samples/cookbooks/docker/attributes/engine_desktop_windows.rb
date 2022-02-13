f01b65a58c5686c75508331d33352fe6e6e4ff7c
Gusztáv Varga <mail@gusztavvargadr.me>
2022-02-13 19:05:37 +0100

    Update Docker versions #132

--- samples/cookbooks/docker/attributes/engine_desktop_windows.rb
+++ samples/cookbooks/docker/attributes/engine_desktop_windows.rb
@@ -16,8 +16,8 @@ default['gusztavvargadr_docker']['engine']['desktop']['windows'] = {
       ],
       'executable' => 'C:/Windows/System32/lxss/tools/kernel',
     },
-    'Docker Desktop 4.4.4' => {
-      'source' => 'https://desktop.docker.com/win/main/amd64/73704/Docker%20Desktop%20Installer.exe',
+    'Docker Desktop 4.5.0' => {
+      'source' => 'https://desktop.docker.com/win/main/amd64/74594/Docker%20Desktop%20Installer.exe',
       'install' => [
         'install',
         '--quiet',
