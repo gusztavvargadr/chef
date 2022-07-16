742f3137d88609ece707b289599f9c9f03c7305a
Gusztáv Varga <mail@gusztavvargadr.me>
2022-07-16 22:52:31 +0200

    Update MSSQL versions #150

--- samples/cookbooks/mssql/test/integration/server-2017-developer/patch_version.rb
+++ samples/cookbooks/mssql/test/integration/server-2017-developer/patch_version.rb
@@ -1,4 +1,4 @@
 describe command('sqlcmd -Q "SELECT @@VERSION"') do
   its('stdout') { should include 'CU29' }
-  its('stdout') { should include '14.0.3436.1' }
+  its('stdout') { should include '14.0.3451.2' }
 end
