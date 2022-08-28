b2c7438679816783dacb3cd036c180262f6821ea
Gusztáv Varga <mail@gusztavvargadr.me>
2022-08-29 00:01:09 +0200

    Consolidate Vagrant settings #123

--- samples/cookbooks/vagrant/attributes/app.rb
+++ samples/cookbooks/vagrant/attributes/app.rb
@@ -2,7 +2,4 @@ default['gusztavvargadr_vagrant']['app_options'] = {
   'latest' => {
     'version' => 'latest',
   },
-  '2.2' => {
-    'version' => '2.2.19',
-  },
 }
--- samples/cookbooks/vagrant/resources/app_ubuntu.rb
+++ samples/cookbooks/vagrant/resources/app_ubuntu.rb
@@ -10,24 +10,11 @@ action :install do
   app_version = new_resource.options['version']
   return if app_version.to_s.empty?
 
-  apt_update '' do
-    action :update
-  end
-
-  apt_package 'apt-transport-https' do
-    action :install
-  end
-
   apt_repository 'hashicorp' do
-    arch 'amd64'
     uri 'https://apt.releases.hashicorp.com'
     key 'https://apt.releases.hashicorp.com/gpg'
     components ['main']
     action :add
-  end
-
-  apt_update '' do
-    action :update
   end
 
   apt_package 'vagrant' do
