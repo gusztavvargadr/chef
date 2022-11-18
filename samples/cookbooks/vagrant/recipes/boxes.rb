28a58dc8d70c3e61cd0121f6f125a60828acacd2
Gusztáv Varga <mail@gusztavvargadr.me>
2022-11-18 20:30:46 +0100

    Fix Vagrant cookbook

--- samples/cookbooks/vagrant/recipes/boxes.rb
+++ samples/cookbooks/vagrant/recipes/boxes.rb
@@ -2,5 +2,6 @@ node['gusztavvargadr_vagrant']['boxes'].each do |box_name, box_options|
   gusztavvargadr_vagrant_box box_name do
     options box_options
     action :add
+    not_if { reboot_pending? }
   end
 end
--- samples/cookbooks/vagrant/recipes/plugins.rb
+++ samples/cookbooks/vagrant/recipes/plugins.rb
@@ -2,5 +2,6 @@ node['gusztavvargadr_vagrant']['plugins'].each do |plugin_name, plugin_options|
   gusztavvargadr_vagrant_plugin plugin_name do
     options plugin_options
     action :install
+    not_if { reboot_pending? }
   end
 end
