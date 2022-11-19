8a0aded33b6271bc87b4c00860a2933dc94e0824
Gusztáv Varga <mail@gusztavvargadr.me>
2022-11-19 16:39:39 +0100

    Consolidate Docker build

--- build/chef/linux/chef-workstation.install.sh
+++ build/chef/linux/chef-workstation.install.sh
@@ -4,8 +4,3 @@ set -e
 curl -Ls https://omnitruck.chef.io/install.sh | bash -s -- -P chef-workstation -v $1
 
 apt-get -y install git
-
-cd ./lib/test-kitchen/kitchen-docker/
-chef gem build --force ./kitchen-docker.gemspec
-chef gem install --force --local --no-user-install ./kitchen-docker-2.11.0.gem
-cd ../../../
