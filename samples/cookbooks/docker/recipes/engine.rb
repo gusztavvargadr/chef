8c470a037d9d09f2553f6a8bb179af00494c1088
Gusztáv Varga <mail@gusztavvargadr.me>
2023-02-20 09:13:48 +0100

    Consolidate Windows reboots

--- samples/cookbooks/docker/recipes/engine.rb
+++ samples/cookbooks/docker/recipes/engine.rb
@@ -1,10 +1,4 @@
 gusztavvargadr_docker_engine '' do
   edition node['gusztavvargadr_docker']['engine']['edition']
-  action :prepare
-end
-
-gusztavvargadr_docker_engine '' do
-  edition node['gusztavvargadr_docker']['engine']['edition']
-  action :install
-  not_if { reboot_pending? }
+  action [ :prepare, :install ]
 end
