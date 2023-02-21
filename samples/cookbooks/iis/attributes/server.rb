774c4ea0ea21e50143e6f26e7372b53317513135
Gusztáv Varga <mail@gusztavvargadr.me>
2023-02-21 02:49:05 +0100

    Update Chef and dependency versions #159

--- samples/cookbooks/iis/attributes/server.rb
+++ samples/cookbooks/iis/attributes/server.rb
@@ -5,24 +5,24 @@ default['gusztavvargadr_iis']['server']['default'] = {
     'IIS-ASPNET' => {},
   },
   'native_packages' => {
-    '.NET 7.0.0 Hosting Bundle' => {
-      'source' => 'https://download.visualstudio.microsoft.com/download/pr/8de163f5-5d91-4dc3-9d01-e0b031a03dd9/0170b328d569a49f6f6a080064309161/dotnet-hosting-7.0.0-win.exe',
+    '.NET 7.0.3 Hosting Bundle' => {
+      'source' => 'https://download.visualstudio.microsoft.com/download/pr/ff197e9e-44ac-40af-8ba7-267d92e9e4fa/d24439192bc549b42f9fcb71ecb005c0/dotnet-hosting-7.0.3-win.exe',
       'install' => [
         '/install',
         '/quiet',
         '/norestart',
       ],
     },
-    '.NET 6.0.11 Hosting Bundle' => {
-      'source' => 'https://download.visualstudio.microsoft.com/download/pr/db07eed5-297a-45b8-bea2-1e93c623a88c/6e5a8d3432e6213f071be3751ae53a08/dotnet-hosting-6.0.11-win.exe',
+    '.NET 6.0.14 Hosting Bundle' => {
+      'source' => 'https://download.visualstudio.microsoft.com/download/pr/321a2352-a7aa-492a-bd0d-491a963de7cc/6d17be7b07b8bc22db898db0ff37a5cc/dotnet-hosting-6.0.14-win.exe',
       'install' => [
         '/install',
         '/quiet',
         '/norestart',
       ],
     },
-    '.NET Core 3.1.31 Hosting Bundle' => {
-      'source' => 'https://download.visualstudio.microsoft.com/download/pr/b84cc7e8-5bf7-4e35-bc2a-94903c823b5c/12f4767bded70bcfb4d56b3a6bfa005d/dotnet-hosting-3.1.31-win.exe',
+    '.NET Core 3.1.32 Hosting Bundle' => {
+      'source' => 'https://download.visualstudio.microsoft.com/download/pr/6744eb9d-dcd4-4386-9d87-b03b70fc58ce/818fadf3f3d919c17ba845b2195bfd9b/dotnet-hosting-3.1.32-win.exe',
       'install' => [
         '/install',
         '/quiet',
--- samples/cookbooks/mssql/test/integration/management-studio-2018/executable_path.rb
+++
@@ -1,3 +0,0 @@
-describe file('C:/Program Files (x86)/Microsoft SQL Server Management Studio 18/Common7/IDE/Ssms.exe') do
-  it { should exist }
-end
