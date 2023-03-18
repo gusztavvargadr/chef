bf8657c6a42858e29333c04178f5a441111d99e3
Gusztáv Varga <mail@gusztavvargadr.me>
2023-03-18 18:41:40 +0100

    Update SSMS version

--- samples/cookbooks/mssql/attributes/management_studio_2019.rb
+++ samples/cookbooks/mssql/attributes/management_studio_2019.rb
@@ -1,7 +1,7 @@
 default['gusztavvargadr_mssql']['management_studio']['2019'] = {
   'native_packages' => {
-    'SSMS 19.0.1' => {
-      'source' => 'https://download.microsoft.com/download/a/3/2/a32ae99f-b6bf-4a49-a076-e66503ccb925/SSMS-Setup-ENU.exe',
+    'SSMS 19.0.2' => {
+      'source' => 'https://download.microsoft.com/download/9/f/8/9f8197f4-0f71-42a3-8717-b2817c77b820/SSMS-Setup-ENU.exe',
       'install' => [
         '/install',
         '/quiet',
