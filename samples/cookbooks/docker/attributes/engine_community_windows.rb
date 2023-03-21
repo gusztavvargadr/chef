0cd6c646d4e727818350a63fe927ae21a7ebc5d0
Gusztáv Varga <mail@gusztavvargadr.me>
2023-03-21 23:39:47 +0100

    Fix Docker install source

--- samples/cookbooks/docker/attributes/engine_community_windows.rb
+++ samples/cookbooks/docker/attributes/engine_community_windows.rb
@@ -3,7 +3,7 @@ default['gusztavvargadr_docker']['engine']['community']['windows'] = {
     'Containers$' => {},
     'Microsoft-Hyper-V$' => {},
   },
-  'powershell_source' => 'https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1',
+  'powershell_source' => 'https://github.com/microsoft/Windows-Containers/blob/82e0a752521b4e3899fc3fa80bc2130c92ade891/helpful_tools/Install-DockerCE/install-docker-ce.ps1',
   'native_packages' => {},
   'compose_source' => 'https://github.com/docker/compose/releases/latest/download/docker-compose-windows-x86_64.exe',
 }
