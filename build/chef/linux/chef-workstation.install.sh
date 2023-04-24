70ce084b1dcc3f572172b7b5f7349e37477ba78e
Gusztáv Varga <mail@gusztavvargadr.me>
2023-04-24 21:37:29 +0000

    Fix installing Git for Chef Workstation

--- build/chef/linux/chef-workstation.install.sh
+++ build/chef/linux/chef-workstation.install.sh
@@ -5,4 +5,6 @@ CHEF_WORKSTATION_VERSION="23.4.1032"
 
 curl -Ls https://omnitruck.chef.io/install.sh | bash -s -- -P chef-workstation -v $CHEF_WORKSTATION_VERSION
 
-apt-get -y install git
+# add-apt-repository ppa:git-core/ppa
+apt-get -y -qq update
+apt-get -y -qq install git
