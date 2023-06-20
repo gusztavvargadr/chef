c5882277cf02eeab110472a3ad0d7fecc3aaf280
Gusztáv Varga <mail@gusztavvargadr.me>
2023-06-20 14:57:24 +0200

    Fix default versions

--- samples/cookbooks/docker/test/integration/community/server_version.rb
+++ samples/cookbooks/docker/test/integration/community/server_version.rb
@@ -1,5 +1,5 @@
 describe command('docker version -f "{{ .Server.Version }}"') do
-  its('stdout') { should include '23.' }
+  its('stdout') { should include '24.' }
 end
 
 describe command('docker version -f "{{ .Server.Platform.Name }}"') do
--- samples/cookbooks/virtualbox/test/integration/default/default.rb
+++ samples/cookbooks/virtualbox/test/integration/default/default.rb
@@ -4,6 +4,6 @@ if os.windows?
   end
 else
   describe command('vboxmanage --version') do
-    its('stdout') { should include '7.0.' }
+    its('stdout') { should include '6.1.' }
   end
 end
