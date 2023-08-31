774430acfba4282d1d6c4425c7c6935d5b8817bb
Gusztáv Varga <mail@gusztavvargadr.me>
2023-08-31 08:58:12 +0200

    Remove Hyper-V from Docker installation

--- samples/cookbooks/docker/attributes/options.rb
+++ samples/cookbooks/docker/attributes/options.rb
@@ -2,7 +2,6 @@ default['gusztavvargadr_docker']['options']['tools']['engine:latest-community'] 
   'windows' => {
     'features' => {
       'Containers$' => {},
-      'Microsoft-Hyper-V$' => {},
     },
     'powershell_source' => 'https://raw.githubusercontent.com/microsoft/Windows-Containers/c344adb529e8fed66e9d16aceb17943b50578b1e/helpful_tools/Install-DockerCE/install-docker-ce.ps1',
     'compose_source' => 'https://github.com/docker/compose/releases/latest/download/docker-compose-windows-x86_64.exe',
