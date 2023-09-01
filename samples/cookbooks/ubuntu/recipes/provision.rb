31f5833388a2355e545d32cc64666bc935b67894
Gusztáv Varga <mail@gusztavvargadr.me>
2023-09-01 08:14:25 +0200

    Consolidate Ubuntu updates

--- samples/cookbooks/ubuntu/recipes/provision.rb
+++ samples/cookbooks/ubuntu/recipes/provision.rb
@@ -2,6 +2,6 @@ updates = node['gusztavvargadr_ubuntu']['updates']
 updates.each do |update_name, update_options|
   gusztavvargadr_ubuntu_update update_name do
     options update_options
-    action update_name
+    action update_name.gsub('-', '_')
   end
 end
