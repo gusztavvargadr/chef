62c1d9d7411fe7ee9664fec1abcce55d3817860b
Gusztáv Varga <mail@gusztavvargadr.me>
2023-11-24 09:42:36 +0100

    Update version numbers for 2311

--- samples/cookbooks/docker/attributes/options.rb
+++ samples/cookbooks/docker/attributes/options.rb
@@ -3,7 +3,7 @@ default['gusztavvargadr_docker']['options']['tools']['engine:latest-community'] 
     'features' => {
       'Containers' => {},
     },
-    'powershell_source' => 'https://raw.githubusercontent.com/microsoft/Windows-Containers/c344adb529e8fed66e9d16aceb17943b50578b1e/helpful_tools/Install-DockerCE/install-docker-ce.ps1',
+    'powershell_source' => 'https://raw.githubusercontent.com/microsoft/Windows-Containers/93a4a835e0a359bb91b11fa30da99c1893f5efd3/helpful_tools/Install-DockerCE/install-docker-ce.ps1',
     'compose_source' => 'https://github.com/docker/compose/releases/latest/download/docker-compose-windows-x86_64.exe',
     'users' => {},
   },
