4cfd446dc71f2340d91cd37e4ec2cee288231a1c
Gusztáv Varga <mail@gusztavvargadr.me>
2024-02-24 13:14:06 +0100

    Fix Chef version check

--- samples/cookbooks/chef/test/integration/default/tools.rb
+++ samples/cookbooks/chef/test/integration/default/tools.rb
@@ -1,3 +1,4 @@
 describe command('chef --version') do
-  its('stdout') { should include 'Chef Workstation version: 24.' }
+  its('stdout') { should include 'Chef Workstation version: 23.' } if os.windows?
+  its('stdout') { should include 'Chef Workstation version: 24.' } unless os.windows?
 end
