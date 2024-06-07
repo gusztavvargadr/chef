eddc4e5c9632711e1d8d6ab104ccfaa93f9c0188
Gusztáv Varga <mail@gusztavvargadr.me>
2024-06-07 12:05:59 +0200

    Update core versions

--- build/chef/linux/chef-workstation.install.sh
+++ build/chef/linux/chef-workstation.install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-CHEF_WORKSTATION_VERSION="24.2.1058"
+CHEF_WORKSTATION_VERSION="24.4.1064"
 
 curl -Ls https://omnitruck.chef.io/install.sh | bash -s -- -P chef-workstation -v $CHEF_WORKSTATION_VERSION
 
