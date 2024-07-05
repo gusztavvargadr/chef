63839f8a68477da360dcb555fbde15762796fafa
Gusztáv Varga <mail@gusztavvargadr.me>
2024-07-05 13:11:12 +0200

    Update policies

--- samples/policies/development-core-tools/Policyfile.rb
+++ samples/policies/development-core-tools/Policyfile.rb
@@ -9,16 +9,19 @@ run_list(
   'recipe[gusztavvargadr_dotnet::initialize]',
   'recipe[gusztavvargadr_hashicorp::initialize]',
   'recipe[gusztavvargadr_docker::initialize]',
+  'recipe[gusztavvargadr_chef::initialize]',
 
   'recipe[gusztavvargadr_git::apply]',
   'recipe[gusztavvargadr_dotnet::apply]',
   'recipe[gusztavvargadr_hashicorp::apply]',
   'recipe[gusztavvargadr_docker::apply]',
+  'recipe[gusztavvargadr_chef::apply]',
 
   'recipe[gusztavvargadr_git::cleanup]',
   'recipe[gusztavvargadr_dotnet::cleanup]',
   'recipe[gusztavvargadr_hashicorp::cleanup]',
   'recipe[gusztavvargadr_docker::cleanup]',
+  'recipe[gusztavvargadr_chef::cleanup]',
 )
 
 attributes(
