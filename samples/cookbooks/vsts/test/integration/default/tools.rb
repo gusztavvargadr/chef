c92efafd26e0873db533d98833b9e6974b095c61
Gusztáv Varga <mail@gusztavvargadr.me>
2024-09-28 23:58:33 +0200

    Fix Kitchen specifications

--- samples/cookbooks/vsts/test/integration/default/tools.rb
+++ samples/cookbooks/vsts/test/integration/default/tools.rb
@@ -1,7 +1,9 @@
-describe directory('C:/Users/vagrant/opt/vsts-agent') do
-  it { should exist } if os.windows?
-end
-
-describe directory('/home/vagrant/opt/vsts-agent') do
-  it { should exist } unless os.windows?
+if os.windows?
+  describe directory("#{os_env('USERPROFILE')}.content}/opt/vsts-agent") do
+    it { should exist }
+  end
+else
+  describe directory("#{os_env('HOME').content}/opt/vsts-agent") do
+    it { should exist }
+  end
 end
