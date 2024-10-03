f4da4f79bd35a08b7ffb037254051f9afcc8f450
Gusztáv Varga <mail@gusztavvargadr.me>
2024-10-03 07:38:00 +0200

    Fix for VirtualBox 7.1

--- samples/cookbooks/virtualbox/resources/tool_ubuntu.rb
+++ samples/cookbooks/virtualbox/resources/tool_ubuntu.rb
@@ -34,14 +34,26 @@ action :install do
   virtualbox_extension_pack_list = shell_out('vboxmanage list extpacks').stdout.strip
   return if virtualbox_extension_pack_list.include?(virtualbox_version)
 
-  virtualbox_extension_pack_download_uri = "https://download.virtualbox.org/virtualbox/#{virtualbox_version}/Oracle_VM_VirtualBox_Extension_Pack-#{virtualbox_version}.vbox-extpack"
-  virtualbox_extension_pack_local_path = "#{Chef::Config['file_cache_path']}/Oracle_VM_VirtualBox_Extension_Pack-#{virtualbox_version}.vbox-extpack"
+  virtualbox_extension_pack_file = 
+    if virtualbox_version.start_with?('7.0')
+      "Oracle_VM_VirtualBox_Extension_Pack-#{virtualbox_version}.vbox-extpack"
+    else
+      "Oracle_VirtualBox_Extension_Pack-#{virtualbox_version}.vbox-extpack"
+    end  
+  virtualbox_extension_pack_download_uri = "https://download.virtualbox.org/virtualbox/#{virtualbox_version}/#{virtualbox_extension_pack_file}"
+
+  virtualbox_extension_pack_local_path = "#{Chef::Config['file_cache_path']}/#{virtualbox_extension_pack_file}"
   remote_file virtualbox_extension_pack_local_path do
     source virtualbox_extension_pack_download_uri
     action :create
   end
 
-  virtualbox_extension_pack_hash = '33d7284dc4a0ece381196fda3cfe2ed0e1e8e7ed7f27b9a9ebc4ee22e24bd23c'
+  virtualbox_extension_pack_hash =
+    if virtualbox_version.start_with?('7.0')
+      '33d7284dc4a0ece381196fda3cfe2ed0e1e8e7ed7f27b9a9ebc4ee22e24bd23c'
+    else
+      'eb31505e56e9b4d0fbca139104da41ac6f6b98f8e78968bdf01b1f3da3c4f9ae'
+    end
   bash 'vboxmanage extpack install' do
     code <<~EOH
       vboxmanage extpack install --replace --accept-license=#{virtualbox_extension_pack_hash} #{virtualbox_extension_pack_local_path}
