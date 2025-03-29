88e51c09a81535a1fa1b0793515892a8b5c87c19
Gusztáv Varga <mail@gusztavvargadr.me>
2025-03-29 22:40:17 +0100

    Update MSSQL, VirtualBox and VSTS versions

--- samples/cookbooks/mssql/test/integration/server-2019-developer/tools.rb
+++ samples/cookbooks/mssql/test/integration/server-2019-developer/tools.rb
@@ -3,6 +3,6 @@ describe command('sqlcmd -Q "SELECT @@VERSION"') do
 end
 
 describe command('sqlcmd -Q "SELECT @@VERSION"') do
-  its('stdout') { should include 'CU28' }
-  its('stdout') { should include '15.0.4385.2' }
+  its('stdout') { should include 'CU32' }
+  its('stdout') { should include '15.0.4430.1' }
 end
