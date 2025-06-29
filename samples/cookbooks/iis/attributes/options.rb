2fe2c598ad663962eaab626f6344b808d3933b16
Gusztáv Varga <mail@gusztavvargadr.me>
2025-06-29 13:33:16 +0200

    Update pending versions

--- samples/cookbooks/iis/attributes/options.rb
+++ samples/cookbooks/iis/attributes/options.rb
@@ -5,16 +5,16 @@ default['gusztavvargadr_iis']['options']['tools']['server:latest'] = {
       'IIS-ASPNET' => {},
     },
     'native_packages' => {
-      '.NET 9.0.5 Hosting Bundle' => {
-        'source' => 'https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/9.0.5/dotnet-hosting-9.0.5-win.exe',
+      '.NET 9.0.6 Hosting Bundle' => {
+        'source' => 'https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/9.0.6/dotnet-hosting-9.0.6-win.exe',
         'install' => [
           '/install',
           '/quiet',
           '/norestart',
         ],
       },
-      '.NET 8.0.16 Hosting Bundle' => {
-        'source' => 'https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/8.0.16/dotnet-hosting-8.0.16-win.exe',
+      '.NET 8.0.17 Hosting Bundle' => {
+        'source' => 'https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/8.0.17/dotnet-hosting-8.0.17-win.exe',
         'install' => [
           '/install',
           '/quiet',
