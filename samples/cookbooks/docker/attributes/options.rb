07513e87d824c46ebcabd36fb616b93727cffa4f
Gusztáv Varga <mail@gusztavvargadr.me>
2025-10-05 01:49:17 +0200

    Consolidate attributes

--- samples/cookbooks/docker/attributes/options.rb
+++ samples/cookbooks/docker/attributes/options.rb
@@ -3,7 +3,7 @@ default['gusztavvargadr_docker']['options']['tools']['engine:latest-community'] 
     'features' => {
       'Containers' => {},
     },
-    'powershell_source' => 'https://raw.githubusercontent.com/microsoft/Windows-Containers/93a4a835e0a359bb91b11fa30da99c1893f5efd3/helpful_tools/Install-DockerCE/install-docker-ce.ps1',
+    'powershell_source' => 'https://raw.githubusercontent.com/microsoft/Windows-Containers/77de763e1c27a466f9ce62e4f74e3039b8f97322/helpful_tools/Install-DockerCE/install-docker-ce.ps1',
     'compose_source' => 'https://github.com/docker/compose/releases/latest/download/docker-compose-windows-x86_64.exe',
     'users' => {},
   },
