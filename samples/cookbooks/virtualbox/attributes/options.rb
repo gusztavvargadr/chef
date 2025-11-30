a8df33fd46863e3ca1bacc3e9f3f1cda07ed0a7b
Gusztáv Varga <mail@gusztavvargadr.me>
2025-12-01 00:44:55 +0100

    Bump VirtualBox versions

--- samples/cookbooks/virtualbox/attributes/options.rb
+++ samples/cookbooks/virtualbox/attributes/options.rb
@@ -1,7 +1,7 @@
 default['gusztavvargadr_virtualbox']['options']['tools']['app:7.2'] = {
   'windows' => {
     'package' => 'virtualbox',
-    'version' => '7.2.2',
+    'version' => '7.2.4',
   },
   'ubuntu' => {
     'package' => 'virtualbox-7.2',
@@ -11,7 +11,7 @@ default['gusztavvargadr_virtualbox']['options']['tools']['app:7.2'] = {
 default['gusztavvargadr_virtualbox']['options']['tools']['app:7.1'] = {
   'windows' => {
     'package' => 'virtualbox',
-    'version' => '7.1.12',
+    'version' => '7.1.14',
   },
   'ubuntu' => {
     'package' => 'virtualbox-7.1',
