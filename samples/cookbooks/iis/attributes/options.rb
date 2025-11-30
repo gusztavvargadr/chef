c5c0df1e10fab700ea375635e04518283432fe75
Gusztáv Varga <mail@gusztavvargadr.me>
2025-12-01 00:49:08 +0100

    Bump IIS versions

--- samples/cookbooks/iis/attributes/options.rb
+++ samples/cookbooks/iis/attributes/options.rb
@@ -5,16 +5,24 @@ default['gusztavvargadr_iis']['options']['tools']['server:latest'] = {
       'IIS-ASPNET' => {},
     },
     'native_packages' => {
-      '.NET 9.0.9 Hosting Bundle' => {
-        'source' => 'https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/9.0.9/dotnet-hosting-9.0.9-win.exe',
+      '.NET 10.0.0 Hosting Bundle' => {
+        'source' => 'https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/10.0.0/dotnet-hosting-10.0.0-win.exe',
         'install' => [
           '/install',
           '/quiet',
           '/norestart',
         ],
       },
-      '.NET 8.0.20 Hosting Bundle' => {
-        'source' => 'https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/8.0.20/dotnet-hosting-8.0.20-win.exe',
+      '.NET 9.0.11 Hosting Bundle' => {
+        'source' => 'https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/9.0.11/dotnet-hosting-9.0.11-win.exe',
+        'install' => [
+          '/install',
+          '/quiet',
+          '/norestart',
+        ],
+      },
+      '.NET 8.0.22 Hosting Bundle' => {
+        'source' => 'https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/8.0.22/dotnet-hosting-8.0.22-win.exe',
         'install' => [
           '/install',
           '/quiet',
