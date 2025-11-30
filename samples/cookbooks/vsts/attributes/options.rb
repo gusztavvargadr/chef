c063e202bd552c7c2aee85acbafe1dbf4e41ef94
Gusztáv Varga <mail@gusztavvargadr.me>
2025-12-01 00:05:09 +0100

    Bump VSTS versions

--- samples/cookbooks/vsts/attributes/options.rb
+++ samples/cookbooks/vsts/attributes/options.rb
@@ -1,6 +1,6 @@
 default['gusztavvargadr_vsts']['options']['tools']['agent:latest'] = {
   'windows' => {
-    'version' => '4.261.0',
+    'version' => '4.264.2',
     'arch' => 'x64',
     'config' => {
       'url' => '',
@@ -12,7 +12,7 @@ default['gusztavvargadr_vsts']['options']['tools']['agent:latest'] = {
     },
   },
   'ubuntu' => {
-    'version' => '4.261.0',
+    'version' => '4.264.2',
     'arch' => 'x64',
     'config' => {
       'url' => '',
