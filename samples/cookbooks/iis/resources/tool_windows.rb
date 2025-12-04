c998d5b589f85eaf2a797bc1d0133e17b3051d6d
Gusztáv Varga <mail@gusztavvargadr.me>
2025-12-04 11:38:23 +0100

    Fix IIS dependency handling

--- samples/cookbooks/iis/resources/tool_windows.rb
+++ samples/cookbooks/iis/resources/tool_windows.rb
@@ -10,18 +10,22 @@ action :install do
   options = node['gusztavvargadr_iis']['options']['tools'][new_resource.name][node['platform']].merge(new_resource.options)
 
   features = options['features']
+  native_packages = options['native_packages']
+
   features.each do |feature_name, _|
     windows_feature_dism feature_name do
       all true
       action :install
+      native_packages.each_key do |native_package_name|
+        notifies :install, "gusztavvargadr_windows_native_package[#{native_package_name}]", :delayed
+      end
     end
   end
 
-  native_packages = options['native_packages']
   native_packages.each do |native_package_name, native_package_options|
     gusztavvargadr_windows_native_package native_package_name do
       options native_package_options
-      action :install
+      action :nothing
     end
   end
 end
