c5d8252c6553e102fdd468d3642371584240415a
Gusztáv Varga <mail@gusztavvargadr.me>
2026-01-30 22:44:38 +0100

    Bump core versions

--- build/chef/windows/chef-client.install.ps1
+++ build/chef/windows/chef-client.install.ps1
@@ -1,4 +1,4 @@
-$CHEF_CLIENT_VERSION="18.8.54"
+$CHEF_CLIENT_VERSION="18.9.4"
 
 . { iwr -useb https://omnitruck.chef.io/install.ps1 } | iex; install -project chef -version $CHEF_CLIENT_VERSION
 
--- build/chef/windows/chef-core.install.ps1
+++ build/chef/windows/chef-core.install.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-$env:chocolateyVersion = "2.5.1"
+$env:chocolateyVersion = "2.6.0"
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 
 rm -Recurse -Force $env:LOCALAPPDATA\Temp\*
